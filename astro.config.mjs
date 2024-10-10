dab1575e016a356e3fe6e681c513ae47f6c40610
Ivan Ivanov <me@ivanov.pro>
2024-10-10 23:17:34 +0300

    update and refactor home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
+
 import icon from "astro-icon"
+import compress from 'astro-compress'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://prevista.github.io',
     compressHTML: true,
     integrations: [
-        mdx(), 
-        icon(), 
-        tailwind({
-            applyBaseStyles: false,
-        }), 
-        compress()
+        icon(),
+        compress(),
+        tailwind(),
     ],
-})
+});
