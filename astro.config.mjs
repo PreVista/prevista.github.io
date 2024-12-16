86ef84d67b3bcbfea33bbb2a4a4a001bc0dbb28a
Ivan Ivanov <me@ivanov.pro>
2024-12-17 00:19:46 +0200

    update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://prevista.github.io',
-    compressHTML: true,
     integrations: [
         icon(),
         compress(),
