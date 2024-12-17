a17be6c5f09c9d5394ce9db99801d6121e1b0510
Ivan Ivanov <me@ivanov.pro>
2024-12-17 15:30:25 +0200

    add our colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            bayern: {
+                'light': '#3B8FD9',
+                'medium': '#123C73',
+                'dark': '#123C73',
+                'white': '#EBEFF2',
+                'eggwhite': '#BACBD9'
+            }
+        }
     },
     plugins: [],
     darkMode: ['class', '.darkmode'],
